f731f65a9d32964c8eeb7c49909adce60ea180bf
darkmian939 <140548399+darkmian939@users.noreply.github.com>
2024-09-23 14:36:17 -0500

    guardadosdjefjdcj

--- src/components/Encuesta/encuesta.js
+++ src/components/Encuesta/encuesta.js
@@ -17,31 +17,11 @@ const Encuesta = () => {
 
     const fetchInstructors = async () => {
       try {
-<<<<<<< HEAD
-        const response = await axios.get(`http://localhost:5000/api/instructor/ficha/${userData.Ficha}`);
-        const instructorsRatedRaw = localStorage.getItem('instructorsRated');
-        const instructorsRated = instructorsRatedRaw ? JSON.parse(instructorsRatedRaw) : [];
-        
-        const filteredInstructorsRated = instructorsRated.filter(item => 
-          item.instructor && item.aprendiz
-        );
-
-        const blockedSet = new Set(
-          filteredInstructorsRated
-            .filter(rating => rating.aprendiz === userData.Nombre)
-            .map(rating => rating.instructor)
-        );
-
-        setBlockedInstructors(blockedSet);
-        setInstructors(response.data);
-=======
         const response = await axios.get(`https://encuestasatisfaccionback-end.onrender.com/api/instructor/ficha/${userData.Ficha}`);
         setInstructors(response.data);
 
         const instructorsRatedRaw = localStorage.getItem('instructorsRated');
-        const instructorsRated = instructorsRatedRaw ? JSON.parse(instructorsRatedRaw) : [];
->>>>>>> 96687f83c8275f47b26c5046372a1c493e682699
-        
+        const instructorsRated = instructorsRatedRaw ? JSON.parse(instructorsRatedRaw) : [];        
         const blockedSet = new Set(
           instructorsRated.filter(rating => rating.aprendiz === userData.Nombre).map(rating => rating.instructor)
         );
@@ -185,44 +165,6 @@ const Encuesta = () => {
 
           {showForm && (
             <div className="form-container">
-<<<<<<< HEAD
-              <h3>Valoración para {selectedName}</h3>
-              <form onSubmit={handleSubmit}>
-                {[
-                  "El Instructor establece relaciones interpersonales cordiales, armoniosas, respetuosas.",
-                  "El Instructor socializa, desarrolla y evalúa la totalidad de los resultados de aprendizaje programados para el semestre.",
-                  "El instructor aplica estrategias participativas de trabajo en equipo que le permiten estar activo permanentemente en su proceso de aprendizaje.",
-                  "El Instructor le orienta su formación mediante un proyecto formativo.",
-                  "El Instructor incentiva al aprendiz a utilizar la plataforma Territorium en el desarrollo de las actividades de aprendizaje.",
-                  "El instructor orienta la formación por medio de guías teniendo en cuenta el proyecto formativo.",
-                  "El Instructor es puntual al iniciar las sesiones.",
-                  "El Instructor demuestra dominio técnico.",
-                  "El Instructor le propone fuentes de consulta (bibliografía, webgrafía…) y ayudas que facilitan su proceso de aprendizaje.",
-                  "El instructor brinda apoyo sobre temáticas del FPI cuando el aprendiz lo requiere y es comprensivo frente a dificultades personales direccionando al área competente.",
-                  "El Instructor revisa y asesora los planes de mejoramiento.",
-                  "El instructor, contribuye al mejoramiento actitudinal del aprendiz en su proceso de formación o El instructor contribuye al mejoramiento del aprendiz en su proceso de formación."
-                ].map((question, index) => (
-                  <div key={index} className="question-container">
-                    <label>{question}</label>
-                    <select 
-                      value={responses[`question-${index}`] || ''}
-                      onChange={(e) => handleResponseChange(index, e.target.value)}
-                      required
-                    >
-                      <option value="">Selecciona una opción...</option>
-                      <option value="Muy Satisfecho">Muy Satisfecho</option>
-                      <option value="Satisfecho">Satisfecho</option>
-                      <option value="Neutro">Neutro</option>
-                      <option value="Insatisfecho">Insatisfecho</option>
-                      <option value="Muy Insatisfecho">Muy Insatisfecho</option>
-                    </select>
-                    <br /> {/* Añadir un espacio entre preguntas */}
-                  </div>
-                ))}
-                <button type="submit" className="submit-button">Enviar</button>
-              </form>
-            </div>
-=======
     <h3>Valoración para {selectedName}</h3>
     <form onSubmit={handleSubmit}>
       {[
@@ -258,7 +200,6 @@ const Encuesta = () => {
       <button type="submit" className="submit-button">Enviar</button>
     </form>
   </div>
->>>>>>> 96687f83c8275f47b26c5046372a1c493e682699
           )}
         </div>
       </div>
